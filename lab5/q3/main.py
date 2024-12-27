a773ec883a0502af75533dfdef346d2778128a17
Simmypeet <simmypeet@outlook.com>
2024-12-27 17:28:46 +0700

    add disk class

---
+++ lab5/q3/main.py
@@ -0,0 +1,16 @@
+class Disk(object):
+    def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
+
+    def showdish(self):
+        pass
+
+    def newpos(self, xpos, ypos):
+        pass
+
+    def cleardisk(self):
+        pass
