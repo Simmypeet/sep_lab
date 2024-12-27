cafc17d8881434d52bc1e76ff651dc9b88c518e3
Diyaan Pulikkal <66010991@kmitl.ac.th>
2024-12-27 17:48:07 +0700

    Update main.py

--- lab5/q3/main.py
+++ lab5/q3/main.py
@@ -1,3 +1,4 @@
+import turtle
 class Disk(object):
     def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
         self.dname = name
@@ -5,12 +6,51 @@ class Disk(object):
         self.dypos = ypos
         self.dheight = height
         self.dwidth = width
+        self.pen = turtle.Turtle()
 
-    def showdish(self):
-        pass
+    def showdisk(self):
+        self.pen.penup()
+        self.pen.setpos(self.dxpos, self.dypos)
+        self.pen.seth(0)
+        self.pen.pendown()
+        self.pen.fillcolor("red")
+        self.pen.pencolor("black")
+        self.pen.begin_fill()
+        self.pen.fd(self.dwidth / 2)
+        self.pen.left(90)
+        self.pen.fd(self.dheight)
+        self.pen.left(90)
+        self.pen.fd(self.dwidth)
+        self.pen.left(90)
+        self.pen.fd(self.dheight)
+        self.pen.left(90)
+        self.pen.fd(self.dwidth / 2)
+        self.pen.end_fill()
+        
 
     def newpos(self, xpos, ypos):
-        pass
+        self.dxpos = xpos
+        self.dypos = ypos
 
     def cleardisk(self):
-        pass
+        self.pen.penup()
+        self.pen.setpos(self.dxpos, self.dypos)
+        self.pen.seth(0)
+        self.pen.pendown()
+        self.pen.fillcolor("white")
+        self.pen.pencolor("white")
+        self.pen.begin_fill()
+        self.pen.fd(self.dwidth / 2)
+        self.pen.left(90)
+        self.pen.fd(self.dheight)
+        self.pen.left(90)
+        self.pen.fd(self.dwidth)
+        self.pen.left(90)
+        self.pen.fd(self.dheight)
+        self.pen.left(90)
+        self.pen.fd(self.dwidth / 2)
+        self.pen.end_fill()
+    
+d = Disk("a", 100, 100)
+d.showdisk()
+d.cleardisk()
