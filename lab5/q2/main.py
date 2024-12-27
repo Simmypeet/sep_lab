88e5cde46f2bf6da36758c14fb759a4badd2395e
Simmypeet <simmypeet@outlook.com>
2024-12-27 17:22:41 +0700

    add bus example

--- lab5/q2/main.py
+++ lab5/q2/main.py
@@ -55,7 +55,8 @@ travel_cost = 0
 trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
          Taxi("KMITL SCB Bank","Ladkrabang Station",5),
          Train("Ladkrabang Station","Payathai Station",40,6),
-         Taxi("Payathai Station","The British Council",3) 
+         Taxi("Payathai Station","The British Council",3) ,
+         Bus("Payathai Station","The British Council",3) 
         ]
 
 for travel in trip:
