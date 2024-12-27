7cfdb3f34cd3df4051148991e38c785c4b9613f7
Simmypeet <simmypeet@outlook.com>
2024-12-27 17:31:50 +0700

    add pole class

--- lab5/q3/main.py
+++ lab5/q3/main.py
@@ -14,3 +14,22 @@ class Disk(object):
 
     def cleardisk(self):
         pass
+
+class Pole(object):
+    def __init__(self, name="", xpos=0, ypos=0, thick=10, length=100):
+        self.pname = name
+        self.stack = []
+        self.toopos = 0
+        self.pxpos = xpos
+        self.pypos = ypos
+        self.pthick = thick
+        self.plength = length
+
+    def showpole(self):
+        pass
+
+    def pushdisk(self, disk):
+        pass
+
+    def popdisk(self):
+        pass
